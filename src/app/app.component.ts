01c0314e14f436737a05ea5194aa76d11f9cf4ec
Rubén Vallejo <rubenvalleho@gmail.com>
2025-02-25 13:26:07 +0100

    Pruebas menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatCardModule],
+  imports: [
+    RouterOutlet,
+    MatCardModule,
+    MatListModule,
+    MatToolbarModule
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
