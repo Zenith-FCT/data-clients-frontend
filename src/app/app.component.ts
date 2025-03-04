2cc68f4cf31a8cd70de4ea88592179d822b6dc00
Rubén Vallejo <rubenvalleho@gmail.com>
2025-03-04 10:39:53 +0100

    Menu pocho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MatCardModule } from '@angular/material/card';
 import { MatListModule } from '@angular/material/list';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { __param } from 'tslib';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -10,11 +12,28 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     RouterOutlet,
     MatCardModule,
     MatListModule,
-    MatToolbarModule
+    MatToolbarModule,
+    TitleCasePipe
     ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'dashboard-cf';
+  sideBarCollapsed = false;
+  activeMenu = 'pedidos';
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+  toggleSideBar(): void {
+    this.sideBarCollapsed = !this.sideBarCollapsed;
+  }
+
+  
+  setActiveMenu(menu: string): void {
+    this.activeMenu = menu;
+  }
 }
