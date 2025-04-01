b4d1978d4a02d532db312bed7ac78c69fd6ad5e1
Juan Camilo Zuñiga Zuñiga <146753356+JCzunzun@users.noreply.github.com>
2025-04-01 09:02:48 +0200

    change model

--- src/app/features/orders-invoices/domain/use-cases/get-invoice-clients-type.use-case.ts
+++ src/app/features/orders-invoices/domain/use-cases/get-invoice-clients-type.use-case.ts
@@ -15,23 +15,31 @@ export class GetInvoiceClientsTypeUseCase {
                 const yearlyData = new Map<string, { 
                     year: string, 
                     recurentTotal: number, 
-                    uniqueTotal: number 
+                    uniqueTotal: number,
+                    totalRecurrentSales: number,
+                    totalUniqueSales: number 
                 }>();
                 
                 invoiceClientsList.forEach(item => {
                     const year = new Date(item.date).getFullYear().toString();
                     const recurent = Number(item.recurent) || 0;
                     const unique = Number(item.unique) || 0;
+                    const totalRecurrentSales = Number(item.totalRecurrentSales) || 0;
+                    const totalUniqueSales = Number(item.totalUniqueSales) || 0;
                     
                     if (yearlyData.has(year)) {
                         const currentYearData = yearlyData.get(year)!;
                         currentYearData.recurentTotal += recurent;
                         currentYearData.uniqueTotal += unique;
+                        currentYearData.totalRecurrentSales += totalRecurrentSales;
+                        currentYearData.totalUniqueSales += totalUniqueSales;
                     } else {
                         yearlyData.set(year, { 
                             year, 
                             recurentTotal: recurent, 
-                            uniqueTotal: unique 
+                            uniqueTotal: unique,
+                            totalRecurrentSales: totalRecurrentSales,
+                            totalUniqueSales: totalUniqueSales
                         });
                     }
                 });
@@ -41,7 +49,9 @@ export class GetInvoiceClientsTypeUseCase {
                         yearData.year,
                         yearData.year,
                         yearData.recurentTotal.toString(),
-                        yearData.uniqueTotal.toString()
+                        yearData.uniqueTotal.toString(),
+                        yearData.totalRecurrentSales.toString(),
+                        yearData.totalUniqueSales.toString()
                     )
                 );
             })
