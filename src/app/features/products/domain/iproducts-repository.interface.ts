4dabf51f4d7e500f593905d9023bd63d92fe8b90
Rubén Vallejo <146753439+Rubenvalleho@users.noreply.github.com>
2025-04-11 19:52:21 +0200

    Grafico creado y optimizado

--- src/app/features/products/domain/iproducts-repository.interface.ts
+++ src/app/features/products/domain/iproducts-repository.interface.ts
@@ -2,9 +2,11 @@ import { Observable } from "rxjs";
 import { TotalBillingPerProductModel } from "./total-billing-per-product.model";
 import { TotalSalesPerProductModel } from "./total-sales-per-product.model";
 import { TopProductModel } from "./top-products.model";
+import { TopProductsByMonthModel } from "./top-products-by-month.model";
 
 export interface IProductsRepository {
     getTotalBillingPerProduct(): Observable<TotalBillingPerProductModel[]>;
     getTotalSalesPerProduct(): Observable<TotalSalesPerProductModel[]>;
     getTopProducts(): Observable<TopProductModel[]>;
+    getTopProductsByMonth(): Observable<TopProductsByMonthModel[]>;
 }
---
+++ src/app/features/products/domain/top-products-by-month.model.ts
@@ -0,0 +1,5 @@
+export interface TopProductsByMonthModel {
+  month: string;
+  productName: string;
+  salesCount: number;
+}
