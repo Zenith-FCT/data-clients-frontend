75951a34695980849e9f5215fe322e2308943743
Javier Lozano <117869190+JavierLozHer@users.noreply.github.com>
2025-05-13 10:13:45 +0200

    Modificar cajas totales

--- src/app/features/coupons/presentation/main-coupons/components/coupon-box/coupon-box.component.ts
+++ src/app/features/coupons/presentation/main-coupons/components/coupon-box/coupon-box.component.ts
@@ -16,16 +16,34 @@ export class CouponBoxComponent implements OnInit {
 
   ngOnInit(): void {
 
-    switch (this.boxType()) {
-      case "coupons":
-        this.viewModel.getTotalCoupons();
-        break;
+  }
 
-      case "discount":
-        this.viewModel.getTotalDiscount();
-        break;
+  getDescription(): string {
+    const month = this.viewModel.month()
+    const year = this.viewModel.year();
+    const totalYears = this.viewModel.totalYears();
 
+    if((this.boxType() == "couponsMonth" || this.boxType() == "discountMonth") && month != "0") {
+      const monthName = this.getMonthName(Number(month));
+      return this.boxType() === 'coupons'
+        ? `Cupones utilizados en ${monthName} del ${year}`
+        : `Total descuento en ${monthName} del ${year}`;
     }
+
+    switch (this.boxType()) {
+      case 'coupons':
+        return totalYears ? 'Total cupones (todos los años)' : `Total cupones en ${year}`;
+      case 'couponsMonth':
+        return  `Total de cupones en ${year}`;
+      case 'discount':
+        return totalYears ? 'Total descuento (todos los años)' : `Total descuento en ${year}`;
+      case 'discountMonth':
+        return `Total descuento en ${year}`;
+    }
+  }
+
+  getMonthName(month: number): string {
+    return new Date(2000, month - 1, 1).toLocaleString('es-ES', { month: 'long' });
   }
 
 }
